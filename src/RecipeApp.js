d3d40346a8fbdcf1624db19df85594c3f4ffc4f1
Romain Testard <40339476+TestardR@users.noreply.github.com>
2018-09-16 19:00:10 +0200

    Add files via upload

---
+++ src/RecipeApp.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Recipe from './Recipe';
+import Navbar from './Navbar';
+import RecipeList from './RecipeList';
+
+import './RecipeApp.css';
+
+class RecipeApp extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <RecipeList />
+      </div>
+    );
+  }
+}
+
+export default RecipeApp;
